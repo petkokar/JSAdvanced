a08b3b6711d574004febda10434139862bce929c
petkokar <petko-kar@abv.bg>
2024-01-22 21:26:09 +0200

    update

--- Objects & Composition/Exercises/02ConstructionCrew.js
+++ Objects & Composition/Exercises/02ConstructionCrew.js
@@ -1,11 +1,9 @@
 function crew(worker) {
 
-    if (worker.dizziness === true) {
+    if (worker.dizziness) {
         let requiredAmount = 0.1 * worker.weight * worker.experience;
         worker.levelOfHydrated += requiredAmount;
-        if (worker.levelOfHydrated >= requiredAmount) {
-            worker.dizziness = false;
-        }
+        worker.dizziness = !worker.dizziness;
     }
     return worker;
 }

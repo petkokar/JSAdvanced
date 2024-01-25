a086bcf2943a8e55db01b188df4f88490608a250
petkokar <petko-kar@abv.bg>
2024-01-25 16:47:01 +0200

    update

--- DOM/Exercises/01. Subtraction/subtract.js
+++ DOM/Exercises/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    const sum = firstNumber - secondNumber;
+    document.getElementById('result').textContent = sum;
 }
--- DOM/Exercises/02. Pascal or Camel Case/solution.js
+++ DOM/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+    let input = document.getElementById('text').value;
+    let currCase = document.getElementById('naming-convention').value;
+    input = input.toLowerCase();
+    input = input.split(' ');
+    let result = '';
+    switch(currCase) {
+      case 'Pascal Case':
+        input.forEach(word => {
+          result += word[0].toUpperCase() + word.substring(1);
+        })
+        break;
+      case 'Camel Case':
+        result = input.shift();
+        input.forEach(word => {
+          result += word[0].toUpperCase() + word.substring(1).toLowerCase();
+        });
+      break;
+      default:
+        result = "Error!"
+        break;
+    }
+
+    document.getElementById("result").textContent = result;
 }

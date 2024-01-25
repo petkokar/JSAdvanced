aeacf24a2ef7c4681cc0b52d49cac30ceb8e9c84
petkokar <petko-kar@abv.bg>
2024-01-25 14:19:14 +0200

    update

--- DOM/Exercises/05. Table - Search Engine/solution.js
+++ DOM/Exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  const searchInput = document.getElementById("searchField");
+  let tableRows = Array.from(document.querySelectorAll("tbody tr"));
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    debugger;
+    let searchText = searchInput.value;
+    if (searchText !== "") {
+      for (let tableRow of tableRows) {
+        let rowCol = Array.from(tableRow.querySelectorAll("td"));
 
-   }
+        for (let data of rowCol) {
+          if (data.textContent.includes(searchText)) {
+            tableRow.classList.add("select");
+            break;
+          } else {
+            tableRow.classList.remove("select");
+          }
+        }
+      }
+    }
+    searchInput.value = "";
+  }
 }

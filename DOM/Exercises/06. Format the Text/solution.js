d67cc63fc302942e2b9b15de6aa088d2874da6b1
petkokar <petko-kar@abv.bg>
2024-01-26 11:48:47 +0200

    Update

--- DOM/Exercises/06. Format the Text/solution.js
+++ DOM/Exercises/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  const inputText = document.getElementById("input");
+  
+  let sentenceArray = inputText.value.split('.').filter(Boolean);
+
+  let pCount = Math.ceil(sentenceArray.length / 3);
+  let htmlParagraphs = '';
+
+  for(let i = 0; i < pCount; i++) {
+    let paragraph = sentenceArray.splice(0, 3).join('. ');
+    paragraph += '.';
+    htmlParagraphs += `<p>${paragraph}</p>`;
+  }
+  
+  document.getElementById('output').innerHTML = htmlParagraphs;
 }
+

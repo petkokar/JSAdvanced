41c146b118f6c82b14ef637e0e41252537fb313d
petkokar <petko-kar@abv.bg>
2024-01-26 02:29:13 +0200

    Update

--- DOM/Exercises/03. Accordion/accordion.js
+++ DOM/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,21 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.getElementsByClassName('button')[0];
+    const accordion = document.getElementById('extra');
+    debugger
+
+    // accordion.style.display= (accordion.style.display === 'none') ? "block" : "none";
+    // button.textContent = (accordion.style.display === 'none') ? "More" : "Less";
+    if (button.textContent === 'More') {
+        button.textContent = 'Less'
+        accordion.style.display = 'block';
+    } else if (button.textContent === 'Less') {
+        button.textContent = 'More'
+        accordion.style.display = 'none';
+    }
+
+    // if (accordion.style.display = 'none') {
+    //     accordion.style.display = 'block';
+    // } else if (accordion.style.display = 'block') {
+    //     accordion.style.display = 'none';
+    // }
 }
--- DOM/Exercises/04. Search in List/search.js
+++ DOM/Exercises/04. Search in List/search.js
@@ -1,3 +1,34 @@
 function search() {
-   // TODO
+   clearResults();
+   const inputText = document.getElementById('searchText').value.toLowerCase();
+   const towns = document.getElementById('towns');
+   const list = towns.getElementsByTagName('li');
+   const array = [];
+   // const matchingTowns = array.filter(town => town === inputText.value);
+
+   for(let city of list)  {
+      const cityName = city.textContent.toLowerCase();
+
+      if (cityName.includes(inputText)){
+         city.style.fontWeight = 'bold';
+         city.style.textDecoration = 'underline';
+
+         array.push(cityName)
+      }
+   }
+
+   document.getElementById('result').textContent = `${array.length} matches found`;
+   function clearResults() {
+      const towns = document.getElementById('towns');
+      const list = towns.getElementsByTagName('li');
+   
+      for (let town of list) {
+          town.style.fontWeight = 'normal';
+          town.style.textDecoration = 'none';
+      }
+   
+      const resultDiv = document.getElementById('result');
+      resultDiv.textContent = '';
+   }
 }
+

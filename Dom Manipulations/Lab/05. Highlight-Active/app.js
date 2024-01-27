b5fadd64f392345ed8370748dd55b0bcdea15837
petkokar <petko-kar@abv.bg>
2024-01-27 13:45:23 +0200

    update

--- Dom Manipulations/Lab/05. Highlight-Active/app.js
+++ Dom Manipulations/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    const childDivs = document.querySelectorAll('div > div');
+    
+    for(let child of childDivs) {
+        const inputFields = child.querySelectorAll('input[type="text"]');
+
+        for(let field of inputFields) {
+            field.addEventListener('focus', function () {
+                this.parentElement.classList.add('focused');
+            });
+
+            field.addEventListener('blur', function () {
+                this.parentElement.classList.remove("focused");
+            })
+        }
+    }
 }
--- Dom Manipulations/Lab/06. Dynamic-Validation/app.js
+++ Dom Manipulations/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.getElementById('email');
+    let value = inputField.value;
+    
+    inputField.addEventListener('change', checkEmail);
+    const reg = /^([\w\-.]+)@([a-z]+)(\.[a-z]+)+$/;
+
+    function checkEmail (event) {
+        if (reg.test(event.target.value)) {
+            event.target.classList.remove('error');
+            return;
+        }
+        event.target.classList = "error";
+    }
 }

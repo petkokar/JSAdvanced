06cb6d53e70fb7335c35051a401e0794865fb99b
petkokar <petko-kar@abv.bg>
2024-01-28 20:10:40 +0200

    update

--- Dom Manipulations/Exercises/05. Encode-and-Decode-Messages/app.js
+++ Dom Manipulations/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeFieldText, decodeFieldText] = document.querySelectorAll('textarea');
+    const [encodeButton, decodeButton] = document.querySelectorAll('button');
+
+    encodeButton.addEventListener('click', () => {
+        let inputText = encodeFieldText.value;
+        inputText = inputText.split('').map(char => {
+            const charCode = char.charCodeAt(0);
+            const encodedCharCode = charCode + 1;
+            return String.fromCharCode(encodedCharCode);
+        }).join('');
+        decodeFieldText.value = inputText;
+        encodeFieldText.value = "";
+    })
+
+    decodeButton.addEventListener('click', () => {
+        let inputText = decodeFieldText.value;
+        inputText = inputText.split('').map(char => {
+            const charCode = char.charCodeAt(0);
+            const decodedCharCode = charCode - 1;
+            return String.fromCharCode(decodedCharCode);
+        }).join('');
+        decodeFieldText.value = inputText;
+    })
+
 }

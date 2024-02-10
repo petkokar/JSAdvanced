2f5d356f9191c0c5b1519d0d74df771677fc645c
petkokar <petko-kar@abv.bg>
2024-02-10 18:48:52 +0200

    Update

--- Prototypes&Inheritance/Exercises/05Company.js
+++ Prototypes&Inheritance/Exercises/05Company.js
@@ -29,7 +29,7 @@ function solution(){
             super(name, age);
             this.tasks = [`${this.name} is working on a complicated task.`,
                         `${this.name} is taking time off work.`,
-                        `${this.name} is supervising junior workers.`] 
+                        `${this.name} is supervising junior workers.`]  
         }
     }
 

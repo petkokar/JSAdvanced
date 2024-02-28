5b9f1360c6d93d39008e92bd2041113fb2f04467
petkokar <petko-kar@abv.bg>
2024-02-28 13:37:07 +0200

    update

--- Async/Exercises/04.Locked-Profile/app.js
+++ Async/Exercises/04.Locked-Profile/app.js
@@ -1,3 +1,52 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const url = "http://localhost:3030/jsonstore/advanced/profiles";
+    const main = document.getElementById("main");
+    main.innerHTML = "";
+
+    const response = await fetch(url);
+    const profilesData = await response.json();
+    let values = Object.values(profilesData);
+    for (let value of values) {
+        let divProfile = document.createElement("div");
+        divProfile.className = "profile";
+
+        divProfile.innerHTML += `
+        <img src="./iconProfile2.png" class="userIcon" />
+				<label>Lock</label>
+				<input type="radio" name="user${value._id}Locked" value="lock" checked>
+				<label>Unlock</label>
+				<input type="radio" name="user${value._id}Locked" value="unlock"><br>
+				<hr>
+				<label>Username</label>
+				<input type="text" name="user${value._id}Username" value="${value.username}" disabled readonly />
+				<div class="user${value._id}Username" style="display: none;">
+                <hr>
+                <label>Email:</label>
+                <input type="email" name="user${value._id}Email" value="${value.email}" disabled readonly />
+                <label>Age:</label>
+                <input type="text" name="user${value._id}Age" value="${value.age}" disabled readonly />
+				</div>
+				
+				<button>Show more</button>
+                `;
+        main.appendChild(divProfile);
+    }
+
+    const buttons = Array.from(document.querySelectorAll("button"));
+    buttons.forEach((x) => x.addEventListener("click", onClickHandler));
+}
+
+function onClickHandler(event) {
+    const button = event.target;
+    const profileDiv = button.closest(".profile");
+    const lockedRadio = profileDiv.querySelector('input[value="lock"]');
+    const hiddenInfoDiv = profileDiv.querySelector("div");
+
+    if (lockedRadio.checked) {
+        return; // Profile is locked, do nothing
+    }
+
+    const showMoreText = button.textContent === "Show more";
+    button.textContent = showMoreText ? "Hide it" : "Show more";
+    hiddenInfoDiv.style.display = showMoreText ? "block" : "none";
 }
--- Async/Exercises/05. Accordion/accordion.js
+++ Async/Exercises/05. Accordion/accordion.js
@@ -1,3 +1,60 @@
 function solution() {
-    //TODO .....
+    document.addEventListener('DOMContentLoaded', async function () {
+        const mainSection = document.getElementById("main");
+        const articlesUrl = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+        const response = await fetch(articlesUrl);
+        const articlesData = await response.json();
+        const reponseValues = Object.values(articlesData);
+
+        for(let data of reponseValues) {
+            let parentDiv = document.createElement('div');
+            parentDiv.className = 'accordion';
+            
+            let headDiv = document.createElement("div");
+            headDiv.className = 'head'
+
+            let spanElement = document.createElement('span');
+            spanElement.textContent = data.title;
+
+            let button = document.createElement("button");
+            button.className = 'button';
+            button.id = data._id;
+            button.textContent = 'More';
+            
+            let extraDivContent = document.createElement('div');
+            extraDivContent.className = 'extra';
+            extraDivContent.style.display = 'none';
+            
+            
+            button.addEventListener('click', async function () {
+                if(button.textContent == 'More') {
+                    const detailsUrl = `http://localhost:3030/jsonstore/advanced/articles/details/${data._id}`;
+                    const detailsResponse = await fetch(detailsUrl);
+                    const detailsData = await detailsResponse.json();
+                    
+                    extraDivContent.innerHTML = '';
+                    
+                    let newPelement = document.createElement('p');
+                    newPelement.textContent = detailsData.content;
+                    extraDivContent.appendChild(newPelement);
+                    extraDivContent.style.display = 'block';
+                    
+                    parentDiv.appendChild(extraDivContent)
+                    button.textContent = 'Less';
+                } else {
+                    extraDivContent.style.display = 'none';
+                    button.textContent = 'More';
+                }
+            });
+            headDiv.appendChild(spanElement);
+            headDiv.appendChild(button);
+            parentDiv.appendChild(headDiv);
+            mainSection.appendChild(parentDiv)
+        }
+
+        
+    })
 }
+
+solution();
